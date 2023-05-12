26ee7c18f4b31def634b31f33e5866f9189b9c09
ellyhonig <39608375+ellyhonig@users.noreply.github.com>
2023-05-12 17:38:19 -0400

    enhanced abstraction

--- Project Parts 1.1 and 1.2 elly.scm
+++ Project Parts 1.1 and 1.2 elly.scm
@@ -1,7 +1,28 @@
 
 
+;;vertex constructor
+(define (make-vertex contents) contents)
+
+;vertexList constructor/selector
+
+
+;takes: vertex , post: (vertex)
+(define (make-vertexList vertex) (list vertex))
+(define (firstVertexInV-List vertexList)(car vertexList))
+(define (restVertexList vertexList)(cdr vertexList))
+;add vertex to vertex list, check for repeats before adding
+;;post: (...vertex...)
+(define (addVertexToVertexList vertexList vertex)
+  
+  (cond((null? vertexList)(make-vertexList vertex))
+
+       ((equal? (firstVertexInV-List vertexList) vertex) vertexList)
+
+       (else (cons (firstVertexInV-List vertexList)(addVertexToVertexList (restVertexList vertexList) vertex)))))
 
 ;;edges constructors/selectors 
+
+;;takes 2 vertices , post (vertex , vertex)
 (define (makeEdge From to)(cons From (list To)))
 
 (define (From edge)(car edge))
@@ -10,45 +31,53 @@
 
 ;;edgeList constructors/ selectors
 
-(define(makeEdgeList) '())
+(define(makeEmptyEdgeList) '())
 ;;add edge to edgelist or make new list, repeats allowed
-(define (addEdge edge edgeList)
+(define (addEdgeToEdgeList edge edgeList)
 
   (cond ((null? edgeList)(list edge))
 
         (else (cons edge edgeList))))
 
 ;adjListblock constructor/ selectors
+
+;takes: index vertexList , post (index vertexList)
 (define (makeAdjListBlock index vertexList )
 (list index vertexList) )
-
+;takes adjlistblock, returns (adjlistblock)
 (define (Index adjListBlock)(car adjListBlock))
 (define (vertexList adjListBlock)(cadr adjListBlock))
 
+;;adjList constructors/ selectors
+(define (makeAdjList adjListBlock)(list adjListBlock))
 (define (firstBlockInAdjList adjList) (car adjList))
 (define (restOfAdjList adjList) (cdr adjList))
 
-;add vertex to vertex list within adjList block, check for repeats before adding
-;;post: (...vertex...)
-(define (addVertexToIndex arry vertex)
-  
-  (cond((null? arry)(list vertex))
 
-       ((equal? (car arry) vertex) (cons (car arry)(cdr arry)))
+;;post ((indexselector ( vertexselector)))
+(define (addEdgeToAdjList edge adjList IndexSelector VertexSelector) 
+  ;is adjList empty? that means our inputted index hasnt been created. Create it.
+  (cond((null? adjList) (makeAdjList (makeAdjListBlock (IndexSelector edge) (addVertexToVertexList '()(VertexSelector edge)))))
+       ;is the indexselector of edge the same as index in first adjblock?
+       ;make a new adjblock with the edge's vertexselector added in, cons it into restofAdjlist
+       ((equal? (IndexSelector edge)(Index (firstBlockInAdjList adjList))) (cons(makeAdjListBlock (IndexSelector edge) (addVertexToVertexList (vertexList(firstBlockInAdjList adjList)) (VertexSelector edge) )) (restOfAdjList adjList)))
+       ;
+       (else (cons(firstBlockInAdjList adjList)(addEdgeToAdjList edge (restOfAdjList adjList)IndexSelector VertexSelector)))))
 
-       (else (cons (car arry)(addVertexToIndex (cdr arry) vertex)))))
 
+
+;;classic accumilate from the notes
 (define (accumulate op init seq)
   (cond((null? seq)init)
        (else(op (car seq)(accumulate op init (cdr seq))))))
 
-;;post ((indexselector ( vertexselector)))
-(define (addEdgeToAdjList edge adjList IndexSelector VertexSelector) 
-  (cond((null? adjList) (list (makeAdjListBlock (IndexSelector edge) (addVertexToIndex '()(VertexSelector edge)))))
-       ((equal? (IndexSelector edge)(Index (firstBlockInAdjList adjList))) (cons(makeAdjListBlock (IndexSelector edge) (addVertexToIndex (vertexList(firstBlockInAdjList adjList)) (VertexSelector edge) )) (restOfAdjList adjList)))
-       (else (cons(firstBlockInAdjList adjList)(addEdgeToAdjList edge (restOfAdjList adjList)IndexSelector VertexSelector)))))
-
+;;just accumilate addEdgeToAdjList over edgeList to get directed graph
 (define(makeDirectedAdjList edgeList) (accumulate (lambda(x y)(addEdgeToAdjList x y From To)) '() edgeList ))
+;;to get undirected, accumilate addEdgeToAdjList over edgeList but flip the index and vertex selector functions. Also have the directed version be the init value in accumilate so-
+;the accumilate call is this: (accumulate addEdgeToAdjList DirectedAdjList edgeList)
 (define(makeUndirectedAdjList edgeList) (accumulate (lambda(x y)(addEdgeToAdjList x y To From))(makeDirectedAdjList edgeList )edgeList))
 
 (define myEdgeList '((a b)(b a)(b 1)(b 2)(1 2)))
+
+(define BuiltEdgeList (addEdgeToEdgeList(makeEdge (make-vertex 'a) (make-vertex 'b)) (makeEmptyEdgeList)))
+(define BuiltAdjList (makeUndirectedAdjList BuiltEdgeList))
